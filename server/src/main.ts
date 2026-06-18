32d471f5d52ed72262b6b12e86fee9b13218ec0f
Raz Butbul <razbutbul@Mac.lan>
2026-06-18 21:37:36 +0300

    docker

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:1111'],
     credentials: true,
   });
 
